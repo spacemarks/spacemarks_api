9aacfd6983528b07fde72b0b04cef3be72874a89
ghar <mohamed.gharsallah.tn@gmail.com>
2021-12-13 10:10:03 +0100

    Create api

---
+++ app.ts
@@ -0,0 +1,38 @@
+import { Oak, OAuth2Client } from './deps.ts'
+
+const PORT = Number(Deno.env.get('PORT'))
+const HOSTNAME = Deno.env.get('HOSTNAME')
+const URL = `${HOSTNAME}:${PORT}`
+const END_REDIRECTION_URI = Deno.env.get('END_REDIRECTION_URI')!
+const oauth2Client = new OAuth2Client({
+  clientId: Deno.env.get('GITHUB_CLIENT_ID')!,
+  clientSecret: Deno.env.get('GITHUB_CLIENT_SECRET'),
+  authorizationEndpointUri: 'https://github.com/login/oauth/authorize',
+  tokenUri: 'https://github.com/login/oauth/access_token',
+  redirectUri: `${URL}/oauth2/callback`,
+  defaults: {
+    scope: 'public_repo',
+  },
+})
+
+const router = new Oak.Router()
+router.get('/status', (ctx) => {
+  ctx.response.body = 'Working...'
+})
+router.get('/login', (ctx) => {
+  ctx.response.redirect(oauth2Client.code.getAuthorizationUri())
+})
+router.get('/oauth2/callback', async (ctx) => {
+  // Exchange the authorization code for an access token
+  const tokens = await oauth2Client.code.getToken(ctx.request.url)
+  const endRedirectionUrl = `${END_REDIRECTION_URI}?accessToken=${encodeURIComponent(tokens.accessToken)}`
+
+  ctx.response.redirect(endRedirectionUrl)
+})
+
+const app = new Oak.Application()
+app.use(router.allowedMethods(), router.routes())
+
+await app.listen({ port: PORT, hostname: HOSTNAME })
+
+console.log(`Server running at ${URL}`)
---
+++ deps.ts
@@ -0,0 +1,2 @@
+export * as Oak from 'https://deno.land/x/oak@v10.0.0/mod.ts'
+export { OAuth2Client } from 'https://deno.land/x/oauth2_client@v0.2.1/mod.ts'
